138f3ba9cac897d55c28176cb672f6455517a74c
Alexey <154370164+Eleksius@users.noreply.github.com>
2026-02-08 23:40:28 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,20 +1,18 @@
-"""Constants for the platformer game."""
-
-# Screen dimensions
+# Разрешение экрана
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Platformer"
 
-# Scaling constants
+# Масштаб объектов
 CHARACTER_SCALING = 0.7
 TILE_SCALING = 0.8
 COIN_SCALING = 1
 
-# Player movement constants
+# Движение игрока
 PLAYER_MOVEMENT_SPEED = 10
 GRAVITY = 1
 PLAYER_JUMP_SPEED = 20
 
-# Player animation constants
+# Анимация
 RIGHT_FACING = 0
 LEFT_FACING = 1
