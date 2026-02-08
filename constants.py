d7f47052264897882032849b6e5af757e18cad48
Alexey <eleksius@mail.ru>
2026-02-08 13:24:29 +0300

    add gitignore

---
+++ constants.py
@@ -0,0 +1,20 @@
+"""Constants for the platformer game."""
+
+# Screen dimensions
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Platformer"
+
+# Scaling constants
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.8
+COIN_SCALING = 1
+
+# Player movement constants
+PLAYER_MOVEMENT_SPEED = 10
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
+
+# Player animation constants
+RIGHT_FACING = 0
+LEFT_FACING = 1
